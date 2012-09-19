fdac4481a7b06173c7fe09cdc8cb9beb8353fa7e
Luiz Guilherme Castilho Martins <luizgui@gmail.com>
2012-09-19 17:23:52 -0300

    voluntario_pessoa_juridica working with historico

--- app/models/voluntario_pessoa_juridica.rb
+++ app/models/voluntario_pessoa_juridica.rb
@@ -7,10 +7,10 @@ validates  :cnpj_pj, :uniqueness => true
 validates  :razao_social_pj, :presence => true
 validates  :inscricao_estadual_pj, :uniqueness => true
 validates  :email_pj, :uniqueness => true
-validates  :telefone_p, :presence => true
-validates  :endereco_p, :presence => true
-validates  :area_atuacao_p, :presence => true
-validates  :contato_p, :presence => true
+validates  :telefone_pj, :presence => true
+validates  :endereco_pj, :presence => true
+validates  :area_atuacao_pj, :presence => true
+validates  :contato_pj, :presence => true
 #validates  :outras_infos_pj
 #validates  :visualizado_pj
 
