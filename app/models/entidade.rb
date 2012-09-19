f10e487052a9df6cc8ffcab23d553894889e5f4c
Luiz Guilherme Castilho Martins <luizgui@gmail.com>
2012-09-19 15:31:17 -0300

    validates cpnj on Entidade like a boss

--- app/models/entidade.rb
+++ app/models/entidade.rb
@@ -1,3 +1,37 @@
 class Entidade < ActiveRecord::Base
   attr_accessible :apoio_estadual_ent, :apoio_federal_ent, :apoio_municipal_ent, :apoio_particular_ent, :aprovado_ent, :atuacao_administrativa_ent, :atuacao_doacao_ent, :atuacao_educacao_ent, :atuacao_juridica_ent, :atuacao_manutencao_ent, :atuacao_outro_ent, :atuacao_recreacao_ent, :atuacao_saude_ent, :cnpj_ent, :data_fundacao_ent, :email_ent, :endereco_ent, :horarios_atendimento_ent, :nome_ent, :nome_responsavel_ent, :numero_beneficiados_ent, :publico_adolescentes_ent, :publico_adultos_ent, :publico_criancas_ent, :publico_especiais_ent, :publico_melhor_idade_ent, :publico_outros_ent, :site_ent, :telefone_ent, :visualizado_ent
+
+  usar_como_cnpj :cnpj_ent
+validates  :cnpj_ent, :uniqueness => true 
+validates  :nome_ent, :presence =>true                  
+validates  :endereco_ent, :presence =>true
+validates  :nome_responsavel_ent, :presence =>true      
+validates  :email_ent, :uniqueness => true                 
+validates  :telefone_ent, :presence =>true              
+/#
+validates  :site_ent                  
+validates  :data_fundacao_ent         
+validates  :publico_criancas_ent      
+validates  :publico_adultos_ent       
+validates  :publico_melhor_idade_ent  
+validates  :publico_adolescentes_ent  
+validates  :publico_especiais_ent     
+validates  :publico_outros_ent        
+validates  :numero_beneficiados_ent   
+validates  :atuacao_juridica_ent      
+validates  :atuacao_administrativa_ent
+validates  :atuacao_recreacao_ent     
+validates  :atuacao_saude_ent         
+validates  :atuacao_educacao_ent      
+validates  :atuacao_manutencao_ent    
+validates  :atuacao_doacao_ent        
+validates  :atuacao_outro_ent         
+validates  :apoio_municipal_ent       
+validates  :apoio_estadual_ent        
+validates  :apoio_federal_ent         
+validates  :apoio_particular_ent      
+validates  :horarios_atendimento_ent  
+validates  :visualizado_ent           
+validates  :aprovado_ent              
+#/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,42 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120919173455) do
+ActiveRecord::Schema.define(:version => 20120919181917) do
+
+  create_table "entidades", :force => true do |t|
+    t.string   "cnpj_ent"
+    t.string   "nome_ent"
+    t.string   "endereco_ent"
+    t.string   "nome_responsavel_ent"
+    t.string   "email_ent"
+    t.string   "telefone_ent"
+    t.string   "site_ent"
+    t.date     "data_fundacao_ent"
+    t.boolean  "publico_criancas_ent"
+    t.boolean  "publico_adultos_ent"
+    t.boolean  "publico_melhor_idade_ent"
+    t.boolean  "publico_adolescentes_ent"
+    t.boolean  "publico_especiais_ent"
+    t.string   "publico_outros_ent"
+    t.integer  "numero_beneficiados_ent"
+    t.boolean  "atuacao_juridica_ent"
+    t.boolean  "atuacao_administrativa_ent"
+    t.boolean  "atuacao_recreacao_ent"
+    t.boolean  "atuacao_saude_ent"
+    t.boolean  "atuacao_educacao_ent"
+    t.boolean  "atuacao_manutencao_ent"
+    t.boolean  "atuacao_doacao_ent"
+    t.string   "atuacao_outro_ent"
+    t.boolean  "apoio_municipal_ent"
+    t.boolean  "apoio_estadual_ent"
+    t.boolean  "apoio_federal_ent"
+    t.boolean  "apoio_particular_ent"
+    t.string   "horarios_atendimento_ent"
+    t.boolean  "visualizado_ent"
+    t.boolean  "aprovado_ent"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+  end
 
   create_table "voluntario_pessoa_fisicas", :force => true do |t|
     t.string   "cpf_pf"
