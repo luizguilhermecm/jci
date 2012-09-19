9975b863f543d31808958bc4f30a9d62160c653d
Luiz Guilherme Castilho Martins <luizgui@gmail.com>
2012-09-19 14:47:48 -0300

    brazilian-rails fixed

--- app/models/voluntario_pessoa_fisica.rb
+++ app/models/voluntario_pessoa_fisica.rb
@@ -1,3 +1,4 @@
 class VoluntarioPessoaFisica < ActiveRecord::Base
   attr_accessible :atuacao_administrativa_pf, :atuacao_doacao_pf, :atuacao_educacao_pf, :atuacao_juridica_pf, :atuacao_manutencao_pf, :atuacao_outro_pf, :atuacao_recreacao_pf, :atuacao_saude_pf, :como_ficou_sabendo_pf, :cpf_pf, :disp_domManha_pf, :disp_domNoite_pf, :disp_domTarde_pf, :disp_quaManha_pf, :disp_quaNoite_pf, :disp_quaTarde_pf, :disp_quiManha_pf, :disp_quiNoite_pf, :disp_quiTarde_pf, :disp_sabManha_pf, :disp_sabNoite_pf, :disp_sabTarde_pf, :disp_segManha_pf, :disp_segNoite_pf, :disp_segTarde_pf, :disp_sexManha_pf, :disp_sexNoite_pf, :disp_sexTarde_pf, :disp_terManha_pf, :disp_terNoite_pf, :disp_terTarde_pf, :email_pf, :experiencia_pf, :nome_pf, :outras_infos_pf, :publico_adolescentes_pf, :publico_adultos_pf, :publico_criancas_pf, :publico_especiais_pf, :publico_melhor_idade_pf, :publico_outros_pf, :telefone_pf, :visuzalizado_pf
+
 end
