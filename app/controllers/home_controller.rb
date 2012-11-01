badaa611bf0a4356f0f13d36bf5245557365b1f3
Luiz Guilherme Castilho Martins <luizgui@gmail.com>
2012-11-01 10:43:21 -0200

    login bug em entidades e PJ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
-
   def index
-    @posts = Post.all
+  	@posts = Post.all
   end
-
+	
   def admin
     render 'admin'
   end
@@ -16,17 +15,5 @@ class HomeController < ApplicationController
     render 'cadastrar'
   end
 
-  def pendencias
-    @entidades = Entidade.where("aprovado_ent = ?", false)
-  end
 
-  def noticias
-    @posts = Post.all
-  end
-
-  def inbox
-    @entidades = Entidade.where("visualizado_ent = ?", false )
-    @voluntario_pessoa_fisicas = VoluntarioPessoaFisica.where("visuzalizado_pf = ?", false)
-    @voluntario_pessoa_juridicas = VoluntarioPessoaJuridica.where("visualizado_pj = ?", false)
- end
 end
